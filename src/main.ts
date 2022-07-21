31fa8906c6ac7706c95c471818b6756a86651bdf
xiguapi211 <xiguapi211@hotmail.com>
2022-07-21 16:12:46 +0800

    add css import in demo page

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
+import '~@boxkit/components/lib/style.css';
 
 createApp(App).mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+/* eslint-disable */
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    resolve: {
+        alias: [{
+            find: /~(.+)/,
+            replacement: resolve(__dirname, 'node_modules/$1')
+        }]
+    },
     plugins: [vue()]
 });
